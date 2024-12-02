b7d69e1cc3259520d81f49c36a4ae2b6040bb96a
Ingrid Gomes <ingridgomespereira98@gmail.com>
2024-12-02 09:27:01 -0300

    Descrição das mudanças feitas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,2 +1,7 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == chute){
+    console.log('Isso aí você descobriu o número secreto (5)');
+}
---
+++ testes/teste.js
@@ -0,0 +1,13 @@
+alert('Boas vindas ao nosso site!');
+let nome='Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro!Em todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+let nomeUsuario = prompt('Qual o seu nome?');
+let idadeUsuário = prompt('Qual a sua idade?');
+if (idadeUsuário>=18){
+    alert('PODE SER PRESO');  
+}
